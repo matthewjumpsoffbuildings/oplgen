980d8eb13fb82a17abba7e059161d20a6f0bb038
Matthew Prasinov <matthewjumpsoffbuildings@gmail.com>
2018-08-23 02:15:06 +0930

    use filenames in output, update chain closing prefix/suffix

--- generate.js
+++ generate.js
@@ -1,36 +1,41 @@
 const fs = require('fs-extra')
+const path = require('path')
 
 const commandLineArgs = require('command-line-args')
 const options = commandLineArgs([
-	{ name: 'numOfSequences', alias: 'n', type: Number, defaultValue: 1000 },
-	{ name: 'sequenceLength', alias: 'l', type: Number, defaultValue: 10 },
+	{ name: 'numOfSequences', alias: 'n', type: Number, defaultValue: 100 },
+	{ name: 'sequenceLength', alias: 'l', type: Number, defaultValue: 5 },
 	{ name: 'outputDirectory', alias: 'o', type: String, defaultValue: "output" },
-	{ name: 'subunitsDirectory', alias: 's', type: String, defaultValue: "subunits" }
+	{ name: 'subunitsDirectory', alias: 's', type: String, defaultValue: "subunits" },
+	{ name: 'delimiter', alias: 'd', type: String, defaultValue: "_" }
 ])
 
 const sequencesNeeded = options.numOfSequences
-const sequenceLengthNeeded = options.sequenceLength
+const sequenceLength = options.sequenceLength
 const outputDirectory = options.outputDirectory
 const subunitsDirectory = options.subunitsDirectory
+const delimiter = options.delimiter
 
 // log out the current settings
-console.log(`Generating ${sequencesNeeded} sequences of length ${sequenceLengthNeeded}`)
+console.log(`\nGenerating ${sequencesNeeded} sequences of length ${sequenceLength}`)
 console.log(`Using subunit SMILES files from the '${subunitsDirectory}' folder`)
 console.log(`Outputting SMILES files into the '${outputDirectory}' folder\n`)
 
 // create a new progress bar instance and use shades_classic theme
 const cliProgress = require('cli-progress');
-const bar = new cliProgress.Bar({}, cliProgress.Presets.shades_classic)
+const bar = new cliProgress.Bar({hideCursor: true, format: 'Progress {bar} {percentage}%  {value}/{total}'}, cliProgress.Presets.shades_classic)
 bar.start(sequencesNeeded, 0)
 
 
 
 // load subunit SMILES
 const subunits = []
+const subunitNames = []
 const subunitFilenames = fs.readdirSync(subunitsDirectory)
 for(var i in subunitFilenames){
 	let data = fs.readFileSync(`${subunitsDirectory}/${subunitFilenames[i]}`, {encoding: 'utf8'})
 	subunits.push(data.trim())
+	subunitNames.push(path.parse(subunitFilenames[i]).name)
 }
 if(subunits.length < 1){
 	console.log(`No subunit SMILES files found in the specified '${subunitsDirectory}' folder`)
@@ -38,8 +43,8 @@ if(subunits.length < 1){
 }
 
 // make sure the specified sequence length is long enough to generate enough unique sequences
-if(sequenceLengthNeeded ** subunits.length < sequencesNeeded){
-	console.log(`Your specified sequence length of ${sequenceLengthNeeded} is too short to generate ${sequencesNeeded} unique sequences`)
+if(sequenceLength ** subunits.length < sequencesNeeded){
+	console.log(`Your specified sequence length of ${sequenceLength} is too short to generate ${sequencesNeeded} unique sequences`)
 	process.exit()
 }
 
@@ -59,7 +64,7 @@ while(sequences.length < sequencesNeeded){
 		sequenceString = ""
 
 	// generate a new random sequence
-	for(var i = 0; i<sequenceLengthNeeded; i++){
+	for(var i = 0; i<sequenceLength; i++){
 		let subunitIndex = Math.floor(Math.random()*subunits.length),
 			subunitString = subunits[subunitIndex]
 
@@ -72,22 +77,29 @@ while(sequences.length < sequencesNeeded){
 	if(sequencesHash.hasOwnProperty(sequenceIndexString))
 		continue
 
+	// generate output filename
+	let filename = ""
+	for(i = 0; i<sequenceLength; i++){
+		filename += subunitNames[sequenceIndexArray[i]]
+		if(i<sequenceLength-1) filename += delimiter
+	}
+
 	// this sequence is new, store all variations in the sequencesHash so it doesnt get repeated
-	for(i = 0; i <sequenceLengthNeeded; i++){
+	for(i = 0; i <sequenceLength; i++){
 		sequenceIndexArray.unshift(sequenceIndexArray.pop())
 		sequenceIndexString = sequenceIndexArray.join(",")
 		sequencesHash[sequenceIndexString] = true
 	}
 
 	// add 1s to make closed loop
-	sequenceString += "1"
-	sequenceString = sequenceString.substr(0, 1) + "1" + sequenceString.substr(1)
+	sequenceString = sequenceString.replace(/^(.)/i, '$&1') // add 1 after first character
+	sequenceString = sequenceString.replace(/\(=O\)$/i, '1(=O)') // add 1 after last (=O)
 
 	// store sequence in array
 	sequences.push(sequenceString)
 
 	// write to SMILES file
-	fs.writeFileSync(`${outputDirectory}/${sequences.length}.smiles`, sequenceString)
+	fs.writeFileSync(`${outputDirectory}/${filename}.smiles`, sequenceString)
 
 	// update progress bar
 	bar.update(sequences.length)
