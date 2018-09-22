88150b09e2d8c10f47e739f206bbc3479d0e9ad5
Matthew Prasinov <matthewjumpsoffbuildings@gmail.com>
2018-09-23 03:23:27 +0930

    bla

--- utils/generate/enumerate.js
+++ utils/generate/enumerate.js
@@ -6,7 +6,11 @@ function enumerate(n, k, c){
 
 	let divisorsArray = divisors(n),
 		necklaces = (1/n) * sum(divisorsArray, (d) => phi(d) * k ** (n/d))
-	
+
+	// let bracelets = (n % 2) ?
+	// 	(necklaces/2) + 0.5 * (k ** ((n+1)/2)) :
+	// 	(necklaces/2) + 0.25 * (k+1) * (k ** (n/2))
+
 	return necklaces
 }
 
