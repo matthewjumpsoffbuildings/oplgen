854cf2be633be3fb4d4552067800191e7aa7938f
Matthew Prasinov <matthewjumpsoffbuildings@gmail.com>
2018-12-05 03:12:05 +1030

    remove unneeded package

--- get-subunits.js
+++ get-subunits.js
@@ -4,7 +4,7 @@ const appRoot = require('app-root-path')
 const jsonPath = appRoot + '/subunits.json'
 
 
-const copyFileSync = require('fs-copy-file-sync')
-copyFileSync(appRoot+'/subunits.json', 'subunits.json')
+const fs = require('fs-extra')
+fs.copySync(appRoot+'/subunits.json', 'subunits.json')
 
 console.log("\n\nCreated subunits.json file\n\n")
