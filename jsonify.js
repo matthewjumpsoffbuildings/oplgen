a048a0a08ab65ac09ff3ca181749dd61782c97fa
Matthew Prasinov <matthewjumpsoffbuildings@gmail.com>
2018-12-06 07:32:44 +1030

    json

--- jsonify.js
+++ jsonify.js
@@ -1,3 +1,20 @@
+/*
+	quick dirty script to convert a bunch of text files each containing a single SMILES and properties, into a subunits.json file
+	example:
+
+	N[C@@H](C[C@@H]1CC[C@H](C=C1)O)C(=O)
+
+	miLogP         -0.77
+	TPSA           98.65
+	natoms         24
+	MW             334.42
+	nON            6
+	nOHNH          4
+	nviolations    0
+	nrotb          4
+	volume         315.53
+*/
+
 const fs = require('fs')
 
 const files = fs.readdirSync('subunits')
@@ -21,7 +38,7 @@ for(var i in files){
 		}
 
 		var found = line.match(/([A-Za-z]*) *([0-9\.\-]*)/)
-		if(found[1] == "" || found[1] == "nviolations") continue
+		if(found[1] == "" || found[1] == "nviolations") continue // ignore nviolations and blank lines
 		props[ found[1] ] = Number(found[2])/2
 	}
 
