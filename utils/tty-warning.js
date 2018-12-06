83a33866a836c3a63a13aa153937482132571a24
Matthew Prasinov <matthewjumpsoffbuildings@gmail.com>
2018-12-07 04:44:05 +1030

    fix tty warning

--- utils/tty-warning.js
+++ utils/tty-warning.js
@@ -1,7 +1,7 @@
 module.exports = function(cmd){
 	if(!process.stderr.isTTY){
 		var ttyMessage = "---- WARNING ---"
-		ttyMessage = "\nYou are running this command in non TTY shell."
+		ttyMessage += "\nYou are running this command in non TTY shell."
 		ttyMessage += "\nThis means the progress bar wont display. The program is still running."
 		ttyMessage += "\nIf you want to see a progress bar, cancel (CTRL-C), and try running in a different shell"
 
