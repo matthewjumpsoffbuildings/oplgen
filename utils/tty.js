c1528f94d0c54466dbd7e2cf276ca6fdc7f8b83b
Matthew Prasinov <matthewjumpsoffbuildings@gmail.com>
2018-12-06 22:59:00 +1030

    tty

--- utils/tty.js
+++ utils/tty.js
@@ -1,15 +1,16 @@
 module.exports = function(cmd){
 	var ttyMessage = false
 
-	if(1 || !process.stderr.isTTY){
-		ttyMessage = "You are running this command in non TTY mode."
+	if(!process.stderr.isTTY){
+		ttyMessage = "You are running this command in non TTY shell."
 		ttyMessage += "\nThis means the progress bar wont display. The program is still running."
+		ttyMessage += "\nIf you want to see a progress bar, cancel (CTRL-C), and try running in a different shell"
 
-		if (1 || process.platform == 'win32') {
-			ttyMessage += "\n\nIf you want to see a progress bar, cancel (CTRL-C), and try running this:"
-			ttyMessage += "\nwinpty "+cmd+" "+process.argv.slice(2).join(" ")
+		if (process.platform == 'win32') {
+			ttyMessage += "\n\nOr you can try running the command like this instead:"
+			ttyMessage += "\nwinpty "+cmd+" "+process.argv.slice(2).join(" ")+"\n"
 		}
 	}
-
+	
 	return ttyMessage
 }
