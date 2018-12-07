6477c3e1baa80910377e954a51cc08c170696de8
Matthew Prasinov <matthewjumpsoffbuildings@gmail.com>
2018-12-08 01:54:38 +1030

    tty

--- utils/tty.js
+++ utils/tty.js
@@ -1,9 +1,7 @@
-module.exports = function(cmd){
-	if(!process.stderr.isTTY){
-		const tty = require('tty').WriteStream.prototype;
-		for(var key in tty){
-			process.stderr[key] = tty[key]
-		}
-		process.stderr.columns = 80
+if(!process.stderr.isTTY){
+	const tty = require('tty').WriteStream.prototype;
+	for(var key in tty){
+		process.stderr[key] = tty[key]
 	}
+	process.stderr.columns = 80
 }
